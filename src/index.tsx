a6f0521dea6082f0a922d4a6307966b7d85b50db
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-05-23 18:14:22 -0700

    removed PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,18 @@
-
 /* @refresh reload */
 import './index.css';
 
 import { render, Suspense } from 'solid-js/web';
 
 import App from './app';
-import { Router } from '@solidjs/router';
+import {Router, RouteSectionProps} from '@solidjs/router';
 import routes from '~solid-pages';
-import { registerSW } from 'virtual:pwa-register';
-
-// Register service worker
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm('New content available. Reload?')) {
-      // Dispatch custom event before updating
-      window.dispatchEvent(new CustomEvent('pwa-update'));
-      updateSW(true); // Explicitly reload the page
-    }
-  },
-  onOfflineReady() {
-    console.log('App ready to work offline');
-    // Dispatch event when offline mode is ready
-    window.dispatchEvent(new CustomEvent('pwa-update'));
-  },
-});
 
 const root = document.getElementById('root');
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-      'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
-  );
+    throw new Error(
+        'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    );
 }
-
-
-
-render(
-    // @ts-ignore
-    () => <Router root={(props) => <App>{props.children}</App>}>{routes}</Router>,
-    root,
-);
+// @ts-ignore
+render(() => <Router root={(props) => <App>{props.children}</App>}>{routes}</Router>, root!);
