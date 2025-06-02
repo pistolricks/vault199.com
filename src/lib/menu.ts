71f5c48ebaeb2a75ce9847d69f7d3432d9ec4da2
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-06-02 13:04:23 -0700

    added

--- src/lib/menu.ts
+++ src/lib/menu.ts
@@ -9,9 +9,13 @@ import misc from "~/static/app/icons/ui/misc.png"
 import status from "~/static/app/icons/ui/status.png";
 import special from "~/static/app/icons/ui/special.png";
 import details from "~/static/app/icons/ui/details.png";
+import events from "~/static/app/icons/ui/events.png";
 
 export const collection = createListCollection({
     items: [
+
+        /*DATA*/
+
         {
             label: '/data', value: [
                 {name: "contacts", href: '/data/contacts', icon: contacts},
@@ -20,6 +24,32 @@ export const collection = createListCollection({
             ]
         },
         {
+          label: '/data/contacts', value: [
+                {name: "contacts", href: '/data/contacts', icon: contacts},
+                {name: "map", href: '/data/map', icon: map},
+                {name: "media", href: '/data/media', icon: events},
+            ]
+        },
+        {
+            label: '/data/map', value: [
+                {name: "contacts", href: '/data/contacts', icon: contacts},
+                {name: "map", href: '/data/map', icon: map},
+                {name: "media", href: '/data/media', icon: events},
+            ]
+        },
+        {
+            label: '/data/media', value: [
+                {name: "contacts", href: '/data/contacts', icon: contacts},
+                {name: "map", href: '/data/map', icon: map},
+                {name: "media", href: '/data/media', icon: events},
+            ]
+        },
+
+
+       /*ITEMS*/
+
+
+        {
             label: '/items', value: [
                 {name: "weapons", href: '/items/weapons', icon: weapons},
                 {name: "armor", href: '/items/armor', icon: armor},
@@ -27,7 +57,54 @@ export const collection = createListCollection({
             ]
         },
         {
+            label: '/items/weapons', value: [
+                {name: "weapons", href: '/items/weapons', icon: weapons},
+                {name: "armor", href: '/items/armor', icon: armor},
+                {name: "misc", href: '/items/misc', icon: misc}
+            ]
+        },
+        {
+            label: '/items/armor', value: [
+                {name: "weapons", href: '/items/weapons', icon: weapons},
+                {name: "armor", href: '/items/armor', icon: armor},
+                {name: "misc", href: '/items/misc', icon: misc}
+            ]
+        },
+        {
+            label: '/items/misc', value: [
+                {name: "weapons", href: '/items/weapons', icon: weapons},
+                {name: "armor", href: '/items/armor', icon: armor},
+                {name: "misc", href: '/items/misc', icon: misc}
+            ]
+        },
+
+
+        /*PROFILE*/
+
+
+        {
             label: '/profile', value: [
+                {name: "status", href: '/profile/status', icon: status},
+                {name: "special", href: '/profile/special', icon: special},
+                {name: "details", href: '/profile/details', icon: details},
+            ]
+        },
+        {
+            label: '/profile/status', value: [
+                {name: "status", href: '/profile/status', icon: status},
+                {name: "special", href: '/profile/special', icon: special},
+                {name: "details", href: '/profile/details', icon: details},
+            ]
+        },
+        {
+            label: '/profile/special', value: [
+                {name: "status", href: '/profile/status', icon: status},
+                {name: "special", href: '/profile/special', icon: special},
+                {name: "details", href: '/profile/details', icon: details},
+            ]
+        },
+        {
+            label: '/profile/details', value: [
                 {name: "status", href: '/profile/status', icon: status},
                 {name: "special", href: '/profile/special', icon: special},
                 {name: "details", href: '/profile/details', icon: details},
