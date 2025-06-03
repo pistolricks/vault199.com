4331e55f218f0ec4b3448179f6112dfb9a80b246
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-06-03 03:28:00 -0700

    added

--- vite.config.ts
+++ vite.config.ts
@@ -66,7 +66,7 @@ export default defineConfig({
                 "name": "Vault-Tec Vault 199",
                 "short_name": "Vault 199",
                 "description": "Pipboy 3000 tested and approved by Vault-Tec",
-                "theme_color": "#28467A",
+                "theme_color": "#323528",
                 "background_color": "#111",
                 "display": "standalone",
                 "orientation": "portrait",
