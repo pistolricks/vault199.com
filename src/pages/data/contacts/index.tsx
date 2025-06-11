8b6bf5ceb0f3103259bad6d7f448ab578b214a79
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-06-10 21:28:05 -0700

    added

--- src/pages/data/contacts/index.tsx
+++ src/pages/data/contacts/index.tsx
@@ -7,6 +7,7 @@ import contacts from "~/static/app/icons/ui/contacts.png"
 import map from "~/static/app/icons/ui/map.png"
 import events from "~/static/app/icons/ui/events.png"
 import {MenuItem} from "~/components/pipboy/footer/bottom-pipboy-menu";
+import CommunicationsApp from "~/components/pipboy/apps/communications/communications-app";
 
 const menuItems: MenuItem[] = [
     {name: "contacts", href: '/data/contacts', icon: contacts},
@@ -19,7 +20,9 @@ type PROPS = {}
 const ContactsSection: Component<RouteSectionProps> = props => {
 
     return (
-      <></>
+      <div class={'relative h-full w-full'}>
+          <CommunicationsApp {...props}/>
+      </div>
     );
 };
 
