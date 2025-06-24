1b9fea716c498022b5f6c5532496a3b770c6a914
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-06-24 13:08:22 -0700

    register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,13 +1,13 @@
-import {Component, createEffect, createMemo} from 'solid-js';
+import {Component} from 'solid-js';
 import TerminalLayout from "~/components/layouts/terminal/terminal-layout";
-import {RouteSectionProps, useSubmission} from "@solidjs/router";
+import {RouteSectionProps} from "@solidjs/router";
 import LoginCard from "~/components/auth/login-card";
 
 const Register: Component<RouteSectionProps> = (props) => {
 
     return (
         <TerminalLayout {...props}>
-            <LoginCard isLogin={false} />
+            <LoginCard isLogin={false}/>
         </TerminalLayout>
 
 
